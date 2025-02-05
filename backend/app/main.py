58b826e57fcba82114e4e31ce2d973330db07021
tzerefos <tzerefos@athenarc.gr>
2025-02-05 11:01:42 +0200

    Use lifespan for beanie initialization.

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,16 +1,26 @@
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
-from db import init_database
 from routes.router import dataset_router
+from beanie import init_beanie
+from motor.motor_asyncio import AsyncIOMotorClient
+from models.datasets import Dataset
+from db import db
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    """Initialize application services."""
+    app.db = AsyncIOMotorClient(db.mongodb_uri).graspos  
+    await init_beanie(app.db, document_models=[Dataset])  
+    print("Startup complete")
+    yield
+    print("Shutdown complete")
+
 
 app = FastAPI(
     title="GRASPOS Catalogue API",
     summary="A simple API",
+    lifespan=lifespan
 )
-
-@app.on_event("startup")
-async def start_db():
-    await init_database()
 
 app.include_router(dataset_router, prefix="/api/datasets")
 
