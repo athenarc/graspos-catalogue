b213d4062c503218e52441cba339f361406b8807
tzerefos <tzerefos@athenarc.gr>
2025-02-05 11:39:06 +0200

    Add jwt and salt generating function

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,16 +1,19 @@
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
-from routes.router import dataset_router
+from routes.dataset import dataset_router
+from routes import user, auth, mail, register
 from beanie import init_beanie
 from motor.motor_asyncio import AsyncIOMotorClient
-from models.datasets import Dataset
+from models.dataset import Dataset
+from models.user import User
 from db import db
+from config import CONFIG
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     """Initialize application services."""
-    app.db = AsyncIOMotorClient(db.mongodb_uri).graspos  
-    await init_beanie(app.db, document_models=[Dataset])  
+    app.db = AsyncIOMotorClient(CONFIG.mongodb_uri).graspos  
+    await init_beanie(app.db, document_models=[Dataset, User])  
     print("Startup complete")
     yield
     print("Shutdown complete")
@@ -23,5 +26,9 @@ app = FastAPI(
 )
 
 app.include_router(dataset_router, prefix="/api/datasets", tags=["Datasets"])
+app.include_router(user.router)
+app.include_router(auth.router)
+app.include_router(mail.router)
+app.include_router(register.router)
 
 
