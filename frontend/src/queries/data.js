0de863101be180784a94bf146621c42c9118e4bf
tzerefos <tzerefos@athenarc.gr>
2025-02-06 09:58:18 +0200

    Modify login queries

--- frontend/src/queries/data.js
+++ frontend/src/queries/data.js
@@ -3,19 +3,21 @@ import axios from "axios";
 
 export function useLogin() {
   return useMutation({
-    mutationFn: ({username, password}) => {
-      return axios.post(`http://localhost:8000/auth/login`, {
+    mutationFn: ({ username, password }) => {
+      return axios.post(process.env.REACT_APP_BACKEND_HOST + `/auth/login`, {
         username,
         password,
       });
-    }
+    },
   });
 }
 
 export function useUserInformation() {
-    return useQuery({
-      queryKey: ["environments"],
-      queryFn: () =>
-        axios.get(`http://localhost:8000/user`).then(({ data }) => data),
-    });
-  }
+  return useQuery({
+    queryKey: ["environments"],
+    queryFn: () =>
+      axios
+        .get(process.env.REACT_APP_BACKEND_HOST + `/user`)
+        .then(({ data }) => data),
+  });
+}
