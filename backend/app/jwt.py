6377c9647875ac3ea0c534276c100b189aa9dafc
tzerefos <tzerefos@athenarc.gr>
2025-02-06 13:16:22 +0200

    Change token expiration time

--- backend/app/jwt.py
+++ backend/app/jwt.py
@@ -7,7 +7,7 @@ from fastapi_jwt import JwtAuthorizationCredentials, JwtAccessBearer, JwtRefresh
 from config import CONFIG
 from models.user import User
 
-ACCESS_EXPIRES = timedelta(minutes=15)
+ACCESS_EXPIRES = timedelta(hours=4)
 REFRESH_EXPIRES = timedelta(days=30)
 
 access_security = JwtAccessBearer(
