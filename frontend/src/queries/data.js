ac8ad14ff393b2f88e28d96b5fe64f55b37992d7
tzerefos <tzerefos@athenarc.gr>
2025-02-15 11:52:16 +0200

    Add axios interceptors to catch token expiration

--- frontend/src/queries/data.js
+++ frontend/src/queries/data.js
@@ -1,11 +1,10 @@
 import { useMutation, useQuery } from "@tanstack/react-query";
-import axios from "axios";
-import useToken from "../useToken";
+import axiosInstance from "./interceptor";
 
 export function useLogin() {
   return useMutation({
     mutationFn: ({ username, password }) => {
-      return axios.post(process.env.REACT_APP_BACKEND_HOST + `auth/login`, {
+      return axiosInstance.post(`auth/login`, {
         username,
         password,
       });
@@ -14,14 +13,12 @@ export function useLogin() {
 }
 
 export function useUpdateUser() {
-  const { token } = useToken();
   return useMutation({
     mutationFn: ({ data }) => {
-      return axios.patch(process.env.REACT_APP_BACKEND_HOST + `user`, data, {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      });
+      return axiosInstance.patch(
+        process.env.REACT_APP_BACKEND_HOST + `user`,
+        data
+      );
     },
   });
 }
@@ -29,22 +26,20 @@ export function useUpdateUser() {
 export function useRegister() {
   return useMutation({
     mutationFn: ({ data }) => {
-      return axios.post(process.env.REACT_APP_BACKEND_HOST + `register`, data);
+      return axiosInstance.post(
+        process.env.REACT_APP_BACKEND_HOST + `register`,
+        data
+      );
     },
   });
 }
 
 export function useUserInformation() {
-  const { token } = useToken();
   return useQuery({
     queryKey: ["user"],
     queryFn: () =>
-      axios
-        .get(process.env.REACT_APP_BACKEND_HOST + `user`, {
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        })
+      axiosInstance
+        .get(`user`, {})
         .then(({ data }) => data)
         .catch((error) => error),
   });
---
+++ frontend/src/queries/interceptor.js
@@ -0,0 +1,34 @@
+import axios from "axios";
+const axiosInstance = axios.create({
+  baseURL: process.env.REACT_APP_BACKEND_HOST,
+  timeout: 1000,
+  headers: { "Content-Type": "application/json" },
+});
+axiosInstance.interceptors.request.use(
+  function (config) {
+    const token = JSON.parse(localStorage.getItem("token"));
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  function (error) {
+    return Promise.reject(error);
+  }
+);
+
+axiosInstance.interceptors.response.use(
+  function (response) {
+    return response;
+  },
+  function (error) {
+    console.log(error);
+    if (error.response && error.response.status === 401) {
+      localStorage.clear();
+      window.location.href = "/";
+    }
+    return Promise.reject(error);
+  }
+);
+
+export default axiosInstance;
