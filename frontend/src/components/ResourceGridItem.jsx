ab4f24d9d3c727c3966d318feddf9e2f58530c0d
tzerefos <tzerefos@athenarc.gr>
2025-03-09 09:51:42 +0200

    Remove unnecessary fields. Improve code layout

---
+++ frontend/src/components/ResourceGridItem.jsx
@@ -0,0 +1,246 @@
+import { useState } from "react";
+
+import {
+  Grid2 as Grid,
+  Card,
+  CardContent,
+  Typography,
+  Stack,
+  Tooltip,
+  IconButton,
+} from "@mui/material";
+
+import {
+  useDeleteDataset,
+  useDeleteDocument,
+  useUpdateDataset,
+  useUpdateDocument,
+  useUserUsername,
+} from "../queries/data";
+import EditIcon from "@mui/icons-material/Edit";
+import CheckCircleIcon from "@mui/icons-material/CheckCircle";
+import CorporateFareIcon from "@mui/icons-material/CorporateFare";
+import PendingActionsIcon from "@mui/icons-material/PendingActions";
+import DeleteIcon from "@mui/icons-material/Delete";
+import ClearIcon from "@mui/icons-material/Clear";
+import Check from "@mui/icons-material/Check";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
+import CloudUploadIcon from "@mui/icons-material/CloudUpload";
+
+export default function ResourceGridItem({ resource, type, user }) {
+  const ownerUsername = useUserUsername(resource?.owner, user);
+  const deleteDatasest = useDeleteDataset();
+  const deleteDocument = useDeleteDocument();
+  const updateDataset = useUpdateDataset(resource._id);
+  const updateDocument = useUpdateDocument(resource._id);
+  const [showDescription, setShowDescription] = useState(false);
+
+  let query = null;
+
+  function handleDelete(id) {
+    if (type === "Document") {
+      query = deleteDocument;
+    } else {
+      query = deleteDatasest;
+    }
+    query.mutate(
+      { id },
+      {
+        onSuccess: (data) => {},
+        onError: (e) => {},
+      }
+    );
+  }
+  function handleUpdate(approved) {
+    if (type === "Document") {
+      query = updateDocument;
+    } else {
+      query = updateDataset;
+    }
+    query.mutate(
+      { approved },
+      {
+        onSuccess: (data) => {},
+        onError: (e) => {},
+      }
+    );
+  }
+  return (
+    <Grid
+      key={resource?._id}
+      component={Card}
+      size={{ xs: 12, md: 3 }}
+      sx={{
+        backgroundColor: "white",
+        borderRadius: "10px",
+        backgroundColor: [type == "Dataset" ? "#FFC067" : "#2B3A57"],
+      }}
+    >
+      <CardContent
+        component={Stack}
+        direction={"row"}
+        justifyContent="space-between"
+        alignItems="center"
+        sx={{ backgroundColor: "white", pb: 0 }}
+      >
+        <Stack direction={"row"} justifyContent="center" spacing={1}>
+          <Typography variant="h6">{resource?.title}</Typography>
+          {resource?.approved ? (
+            <Tooltip title="Resource has been approved by an Admin">
+              <CheckCircleIcon color="success" fontSize="small" />
+            </Tooltip>
+          ) : (
+            <Tooltip title="Resource is pending approval by an Admin">
+              <PendingActionsIcon color="warning" fontSize="small" />
+            </Tooltip>
+          )}
+        </Stack>
+
+        <CardContent
+          component={Stack}
+          direction={"row"}
+          spacing={0}
+          sx={{ p: "0!important" }}
+        >
+          {!resource.approved ? (
+            <>
+              <Tooltip title={"Approve " + String(type)}>
+                <IconButton
+                  color="success"
+                  onClick={() => {
+                    handleUpdate(true);
+                  }}
+                >
+                  <Check fontSize="" />
+                </IconButton>
+              </Tooltip>
+              <Tooltip
+                title={
+                  "Reject " +
+                  String(type) +
+                  ". " +
+                  String(type) +
+                  " will be deleted"
+                }
+              >
+                <IconButton
+                  color="error"
+                  onClick={() => {
+                    handleUpdate(false);
+                  }}
+                >
+                  <ClearIcon />
+                </IconButton>
+              </Tooltip>
+            </>
+          ) : (
+            <>
+              <Tooltip title={"Edit " + String(type)}>
+                <div>
+                  <IconButton disabled={!user}>
+                    <EditIcon />
+                  </IconButton>
+                </div>
+              </Tooltip>
+              <Tooltip title={"Delete " + String(type)}>
+                <div>
+                  <IconButton
+                    color="error"
+                    disabled={!user}
+                    onClick={() => {
+                      handleDelete(resource._id);
+                    }}
+                  >
+                    <DeleteIcon />
+                  </IconButton>
+                </div>
+              </Tooltip>
+            </>
+          )}
+        </CardContent>
+      </CardContent>
+
+      <CardContent
+        component={Stack}
+        direction={"row"}
+        justifyContent="space-between"
+        alignItems="center"
+        spacing={1}
+        sx={{
+          py: 1,
+          pb: 0,
+          backgroundColor: "white",
+          borderBottom: "1px solid grey",
+        }}
+      >
+        <Typography variant="subtitle1" sx={{ fontStyle: "italic" }}>
+          More Information
+        </Typography>
+        <IconButton
+          onClick={() => {
+            setShowDescription(!showDescription);
+          }}
+        >
+          {showDescription && <KeyboardArrowUpIcon />}
+          {!showDescription && <KeyboardArrowDownIcon />}
+        </IconButton>
+      </CardContent>
+      {showDescription && (
+        <CardContent
+          sx={{
+            backgroundColor: "beige",
+            p: 2,
+            height: "50px",
+            overflow: "auto",
+          }}
+        >
+          {resource?.description}
+        </CardContent>
+      )}
+      <CardContent
+        component={Stack}
+        direction={"row"}
+        spacing={1}
+        justifyContent="space-between"
+        alignItems="center"
+        sx={{ backgroundColor: "white", pt: 3 }}
+      >
+        <CardContent
+          component={Stack}
+          direction={"row"}
+          spacing={1}
+          sx={{ p: "0!important" }}
+        >
+          <CloudUploadIcon />
+          <Typography>{ownerUsername?.data?.data?.username}</Typography>
+        </CardContent>
+        <CardContent
+          component={Stack}
+          direction={"row"}
+          spacing={1}
+          sx={{ p: "0!important" }}
+        >
+          <CorporateFareIcon />
+          <Typography>
+            {resource?.organization ? resource?.organization : "N/A"}
+          </Typography>
+        </CardContent>
+      </CardContent>
+      <CardContent
+        component={Typography}
+        variant={"subtitle2"}
+        sx={{
+          p: 0.5,
+          paddingBottom: "4px !important;",
+          color: "white",
+          textAlign: "center",
+          m: "auto",
+          height: "22px",
+        }}
+      >
+        {type.toUpperCase()}
+      </CardContent>
+    </Grid>
+  );
+}
