f3c2c6d19d7d6a2163f5117404a41aeffc5790e7
tzerefos <tzerefos@athenarc.gr>
2025-03-25 14:20:28 +0200

    Update submit animation in forms

--- frontend/src/components/Forms/ProfileForm.jsx
+++ frontend/src/components/Forms/ProfileForm.jsx
@@ -211,8 +211,8 @@ export default function ProfileForm() {
             <Button
               type="submit"
               variant="contained"
-              disabled={updateUser.isLoading}
-              loading={updateUser.isLoading}
+              disabled={updateUser.isPending}
+              loading={updateUser.isPending}
               endIcon={<SaveIcon />}
               loadingPosition="end"
               sx={{ backgroundColor: "#20477B" }}
