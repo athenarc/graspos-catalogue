bc7fd090f8eb0a3c489bf7d25bdf4730ac15a85f
tzerefos <tzerefos@athenarc.gr>
2025-10-07 09:14:41 +0300

    Remove doi from document form fields

--- frontend/src/components/Forms/DocumentsFormFields.jsx
+++ frontend/src/components/Forms/DocumentsFormFields.jsx
@@ -1,31 +1,9 @@
-import {
-  Stack,
-  InputLabel,
-  Select,
-  MenuItem,
-  FormControl,
-  TextField,
-} from "@mui/material";
+import { Stack } from "@mui/material";
 
 export default function DocumentFormFields({ form }) {
   return (
     <Stack direction="column" sx={{ mt: 2 }}>
-      <Stack direction="row" useFlexGap spacing={2} sx={{ mt: 2 }}>
-        <TextField
-          {...form?.register("doi", {
-            required: "DOI is required",
-            pattern: {
-              value: /^10\.\d{4,9}\/[-._;()/:A-Z0-9]+$/i,
-              message: "Not a valid DOI",
-            },
-          })}
-          label="DOI"
-          error={!!form?.formState?.errors?.doi}
-          helperText={form?.formState?.errors?.doi?.message ?? " "}
-          fullWidth
-          required
-        />
-      </Stack>
+      <Stack direction="row" useFlexGap spacing={2} sx={{ mt: 2 }}></Stack>
     </Stack>
   );
 }
