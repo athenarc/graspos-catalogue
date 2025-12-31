d3f493d4247e8e5db8b3ede5ef3a2d8a9441d89e
tzerefos <tzerefos@athenarc.gr>
2025-12-31 13:30:16 +0200

    Provide assessment functionality filter & input options through global options file.

--- frontend/src/components/Forms/Fields/Coverage/AssessmentFunctionalities.jsx
+++ frontend/src/components/Forms/Fields/Coverage/AssessmentFunctionalities.jsx
@@ -10,60 +10,15 @@ import {
   FormControl,
 } from "@mui/material";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import { assessmentFunctionalityMenuItems } from "@helpers/MenuItems";
 
 export default function AssessmentFunctionalities({
   form,
   resource = null,
   resource_type = null,
 }) {
-  const menuItems = [
-    {
-      id: 1,
-      value: "scholarly_data_enrichment_missing_attributes",
-      label: "Scholarly data enrichment: Missing attributes",
-      resource_types: ["all"],
-    },
-    {
-      id: 2,
-      value: "scholarly_data_enrichment_indicators",
-      label: "Scholarly data enrichment: Indicators",
-      resource_types: ["all"],
-    },
-    {
-      id: 3,
-      value: "scholarly_data_enrichment_semantics",
-      label: "Scholarly data enrichment: Missing links & semantics",
-      resource_types: ["all"],
-    },
-    {
-      id: 4,
-      value: "open_science_monitoring_researchers",
-      label: "Open Science monitoring: Researchers",
-      resource_types: ["all"],
-    },
-    {
-      id: 5,
-      value: "open_science_monitoring_institutions",
-      label: "Open Science monitoring: Institutions",
-      resource_types: ["all"],
-    },
-    {
-      id: 6,
-      value: "open_science_monitoring_countries",
-      label: "Open Science monitoring: Countries",
-      resource_types: ["all"],
-    },
-    {
-      id: 7,
-      value: "open_science_monitoring_general",
-      label: "Open Science monitoring: General",
-      resource_types: ["all"],
-    },
-    { id: 8, value: "data", label: "Data", resource_types: ["service"] },
-    { id: 9, value: "other", label: "Other", resource_types: ["all"] },
-  ];
 
-  const filteredOptions = menuItems.filter(
+  const filteredOptions = assessmentFunctionalityMenuItems.filter(
     (item) =>
       item.resource_types.includes("all") ||
       item.resource_types.includes(resource_type)
