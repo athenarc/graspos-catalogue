5e2acdbc65eb51b7a1d21d45d882e932f0e35148
tzerefos <tzerefos@athenarc.gr>
2026-03-30 13:22:04 +0300

    Add footer component. Adjust padding for mobile responsiveness.

--- frontend/src/components/Layout/AppLayout.jsx
+++ frontend/src/components/Layout/AppLayout.jsx
@@ -1,13 +1,16 @@
 import { useAuth } from "../AuthContext";
 import BaseLayout from "./BaseLayout";
+import Footer from "@components/Layout/Footer";
 
 export default function AppLayout() {
   const { handleLogin, handleLogout, user } = useAuth();
   return (
-    <BaseLayout
-      user={user}
-      handleLogout={handleLogout}
-      handleLogin={handleLogin}
-    />
+    <>
+      <BaseLayout
+        user={user}
+        handleLogout={handleLogout}
+        handleLogin={handleLogin}
+      />
+    </>
   );
 }
